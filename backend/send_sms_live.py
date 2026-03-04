96247adee7a32ffca19d3871986a03660fd40ae9
Brian Ortega <Ibrain532@gmail.com>
2026-03-04 18:04:29 -0500

    refactor(ui): hide Signin button in navbar if user is loggedin

--- backend/send_sms_live.py
+++ backend/send_sms_live.py
@@ -8,12 +8,12 @@ twilio_bp = Blueprint("twilio", __name__)
 load_dotenv()
 
 # live crendentials
-account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+# account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+# auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
 # test credentials
-# account_sid = os.getenv("TWILIO_ACCOUNT_SID_TEST")
-# auth_token = os.getenv("TWILIO_AUTH_TOKEN_TEST")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID_TEST")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN_TEST")
 
 #When using test credentials, this is the only phone number that makes sending messages give successful response
 from_phone_number = os.getenv("TWILIO_PHONE_NUMBER_TEST")
@@ -71,10 +71,12 @@ def send_order_status_sms(order, phone_number):
 
     message = f"Your order #{order.get('orderId')} is now {order.get('status') }."
     
-    virtual_phone_number = '+18777804236' 
+    # virtual_phone_number = '+18777804236' 
 
     client.messages.create(
         body = message,
-        from_ = '+18449833971',
-        to = virtual_phone_number
+        # from_ = '+18449833971',
+        # to = virtual_phone_number
+        from_ = str(from_phone_number),
+        to = phone_number
     )

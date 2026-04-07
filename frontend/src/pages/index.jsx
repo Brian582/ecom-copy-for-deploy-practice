35f3cb525f10e674f3cda509ed8512dfeef85a46
Brian Ortega <Ibrain532@gmail.com>
2026-04-07 18:00:08 -0400

    renamed index.css file for vercel deployment

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/Hero';
 import FeaturedDishes from '../components/FeaturedDishes';
 import CallToAction from '../components/CallToAction';
-import '../styles/Index.css';
+import '../styles/index.css';
 
 export default function Index() {
   return (
